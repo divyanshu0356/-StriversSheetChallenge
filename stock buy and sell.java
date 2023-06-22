74de67269932317ead82b3d3a5d4e65bddfba740
Divyanshu Kumar <134629130+divyanshu0356@users.noreply.github.com>
2023-06-22 23:40:29 +0530

    Add files via upload

---
+++ stock buy and sell.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int mini=prices[0];
+        for(int i=1;i<prices.length;i++)
+        {
+            profit=Math.max(profit,prices[i]-mini);
+            mini=Math.min(mini,prices[i]);
+        }
+        return profit;
+        
+    }
+}
